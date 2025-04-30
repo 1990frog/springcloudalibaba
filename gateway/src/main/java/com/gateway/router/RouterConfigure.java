abb39470241cc5b441d68638c40215096ccbb8b0
caijingquan <1990frog@gmail.com>
2025-04-30 15:44:45 +0800

    commit

--- gateway/src/main/java/com/gateway/router/RouterConfigure.java
+++ gateway/src/main/java/com/gateway/router/RouterConfigure.java
@@ -1,5 +1,6 @@
 package com.gateway.router;
 
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +9,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouterConfigure {
 
+    // @Bean
+    // public RouteLocator customRoute(RouteLocatorBuilder builder) {
+    //     return builder.routes()
+    //             .route("producer", r -> r
+    //                     .path("/test/**")
+    //                     .filters(f -> f.stripPrefix(1))
+    //                     .uri("http://localhost:8080/producer"))
+    //             .build();
+    // }
+
     @Bean
-    public RouteLocator customRoute(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("producer", r -> r
-                        .path("/test/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8080/producer"))
-                .build();
+    public GlobalFilter loggingFilter() {
+        return (exchange, chain) -> {
+            System.out.println("请求路径：" + exchange.getRequest().getURI());
+            return chain.filter(exchange);
+        };
     }
 
 }
